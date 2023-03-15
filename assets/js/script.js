75a9b4872590c3388f4e3e27442ac50fa5042a34
Vinícius de Morais <95432848+viniciusdmorais@users.noreply.github.com>
2023-03-15 17:07:13 -0300

    ajuste para as funcoes dos contatos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@ const homeLink = document.getElementById('nav-home');
 const aboutLink = document.getElementById('nav-sobre');
 const experienceLink = document.getElementById('nav-experiencias');
 const skillsLink = document.getElementById('nav-conhecimentos');
+const contatoLink = document.getElementById('nav-contato');
 
 const aboutSection = document.getElementById('sobre');
 const experienceSection = document.getElementById('experiencias');
 const skillsSection = document.getElementById('conhecimentos');
+const contatoSection = document.getElementById('formulario-contato');
 
 const linkInsta = document.getElementById('img-instagram')
 const linkLinkedin = document.getElementById('img-linkedin')
@@ -35,6 +37,20 @@ skillsLink.addEventListener('click', () => {
     skillsSection.scrollIntoView({
         behavior: 'smooth'
     });
+});
+
+contatoLink.addEventListener('click', () => {
+    if (contatoSection.style.display == ''){
+    contatoSection.style.display = 'flex';
+    contatoSection.style.justifyContent = 'end';
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}else{
+    contatoSection.style.display = '';
+    contatoSection.style.justifyContent = ''
+}
 });
 
 linkInsta.addEventListener("click", function () {
