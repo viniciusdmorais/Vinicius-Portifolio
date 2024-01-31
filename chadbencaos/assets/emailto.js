1e538dab81d8eeedd06608dd794627ea24f5ae0c
Vinícius de Morais <95432848+viniciusdmorais@users.noreply.github.com>
2024-01-31 17:29:26 -0300

    Update emailto.js

--- chadbencaos/assets/emailto.js
+++ chadbencaos/assets/emailto.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById('botao-enviar');
+const form = document.getElementById('form-contatos');
 
 document.getElementById('form-contatos')
     .addEventListener('submit', function (event) {
@@ -13,10 +14,15 @@ document.getElementById('form-contatos')
         emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
                 btn.value = 'Send Email';
-                alert('Sent!');
+                alert('Mensagem enviada!');                
+                 // Limpar os campos do formulário
+                form.reset();
+                alert('Agora, confira a sugestão de presentes')
             }, (err) => {
                 btn.value = 'Send Email';
                 alert(JSON.stringify(err));
+                // Limpar os campos do formulário
+                form.reset();
             });
             
     });
