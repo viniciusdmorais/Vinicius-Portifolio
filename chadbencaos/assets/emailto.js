37de87b9da34939b748dace9a980a2fc4e56fe83
Vinícius de Morais <95432848+viniciusdmorais@users.noreply.github.com>
2024-02-01 14:28:50 -0300

    Update emailto.js

--- chadbencaos/assets/emailto.js
+++ chadbencaos/assets/emailto.js
@@ -1,6 +1,17 @@
 const btn = document.getElementById('botao-enviar');
 const form = document.getElementById('form-contatos');
 
+const iniLink = document.getElementById('nav-inicio');
+const mensagemLink = document.getElementById('nav-mensagem');
+const itensLink = document.getElementById('nav-itens');
+const qrcodesLink = document.getElementById('nav-qrcodes');
+const dadosentregaLink = document.getElementById('nav-dados-entrega');
+
+const mensagemSection = document.getElementById('formulario-contato');
+const itensSection = document.getElementById('itens-lista');
+const qrcodeSection = document.getElementById('qrcodes');
+const dadosentregaSection = document.getElementById('dadosentrega');
+
 document.getElementById('form-contatos')
     .addEventListener('submit', function (event) {
 
@@ -26,3 +37,34 @@ document.getElementById('form-contatos')
             });
             
     });
+
+iniLink.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+mensagemLink.addEventListener('click', () => {
+    mensagemSection.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+itensLink.addEventListener('click', () => {
+    itensSection.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+qrcodesLink.addEventListener('click', () => {
+    qrcodeSection.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+dadosentregaLink.addEventListener('click', () => {
+    dadosentregaSection.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
